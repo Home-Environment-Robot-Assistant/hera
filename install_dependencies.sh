8542ecbacba726a3e13281cb442bac97d07981ca
fagnerpimentel <fagnerpimentel@gmail.com>
2022-05-03 14:28:57 -0300

    first commit with code developed by robofei team

---
+++ install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get install -y xterm
+apt-get install -y ros-melodic-teleop-twist-keyboard
+apt-get install -y ros-melodic-gmapping
+apt-get install -y ros-melodic-map-server
+apt-get install -y ros-melodic-move-base
+apt-get install -y ros-melodic-social-navigation-layers
+apt-get install -y ros-melodic-amcl
+apt-get install -y ros-melodic-fake-localization
+apt-get install -y ros-melodic-carrot-planner
+apt-get install -y ros-melodic-global-planner
+apt-get install -y ros-melodic-dwa-local-planner
+apt-get install -y ros-melodic-eband-local-planner
+apt-get install -y ros-melodic-teb-local-planner
